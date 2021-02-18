68f3d4937c03c0e1f5bd7f403e74095fc27401a1
Jonathan O'Connor <JohnPierreOConnor@gmail.com>
2021-02-18 10:42:21 -0500

    implemented reservation functionality

--- server.js
+++ server.js
@@ -4,35 +4,37 @@ const app = express();
 const PORT = 3000; // for Heroku ? process.env.PORT || 3000
 
 // will share any static html files with the browser
-app.use( express.static('html') );
+app.use(express.static('html'));
 // accept incoming POST requests
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 
 // Data =======================================================
-let tableList = [ { name: 'test table', phone: '999-999-9999'} ]
+let tableList = [{ name: 'test table', phone: '999-999-9999' },{ name: 'test table', phone: '999-999-9999' },{ name: 'test table', phone: '999-999-9999' },{ name: 'test table', phone: '999-999-9999' },{ name: 'test table', phone: '999-999-9999' },{ name: 'test table6', phone: '999-999-9999' }]
 
 // Routes (Endpoints) =========================================
-app.get('/api/tables/:tableId', function(req, res) {
+app.get('/api/tables/:tableId', function (req, res) {
     const tableId = req.params.tableId;
     // get the table info
     const tableData = {};
-    res.send( tableData );
+    res.send(tableData);
 });
 
-app.get('/api/tables', function(req, res) {
-    res.send( tableList );
+app.get('/api/tables', function (req, res) {
+    res.send(tableList);
 });
 
-app.post('/api/tables/reserve', function(req, res) {
-    console.log( '[reservation] req.body: ', req.body );
+app.post('/api/tables/reserve', function (req, res) {
+    console.log('[reservation] req.body: ', req.body);
 
     const newTableData = req.body;
-    res.send( { message: `Saved: ${newTableData.name}` } );
+    tableList.push(newTableData)
+    console.log(tableList)
+    res.send({message: `done`})
 });
 
 // Listener ==================================================
-app.listen(PORT, function() {
+app.listen(PORT, function () {
     console.log('Serving hot-tables on PORT ' + PORT);
 });
